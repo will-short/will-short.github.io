b19ba61cd2a98ea247376ade8c13798373b2f5d1
will-short <willofmetal85@gmail.com>
2022-01-05 18:55:16 -0600

    added about scroll effects

--- index.js
+++ index.js
@@ -6,4 +6,43 @@ window.onload = () => {
     const active = document.getElementsByClassName(type)[0];
     active.classList.add("active");
   };
+
+  const lines = document.querySelectorAll(".line");
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.style.animation = `anim1 2s forwards ease-out`;
+      } else {
+        entry.target.style.animation = "none";
+      }
+    });
+  });
+  observer2 = new IntersectionObserver((entry) => {
+    if (entry[0].intersectionRatio > 0) {
+      entry[0].target.style.animation = `anim2 2s forwards linear`;
+    } else {
+      entry[0].target.style.animation = "none";
+    }
+  });
+  observer3 = new IntersectionObserver((entry) => {
+    if (entry[0].intersectionRatio > 0) {
+      entry[0].target.style.animation = `anim3 2s forwards linear`;
+      setTimeout(() => {
+        const items = document.querySelectorAll(".item");
+        items.forEach((item, i) => {
+          setTimeout(() => {
+            item.style.animation = `anim4 1s forwards linear`;
+          }, 100 * i);
+        });
+      }, 2000);
+    } else {
+      entry[0].target.style.animation = "none";
+    }
+  });
+  observer3.observe(right);
+  observer2.observe(left);
+  lines.forEach((line) => observer.observe(line));
 };
